c7f9d5bde254b707e2e8efa53d43d78775225358
Richard Dahl <richard@dahl.us>
2024-09-25 21:01:28 -0400

    fix: change fixture scope from class to function and improve database cleanup in tests

--- tests/auth/conftest.py
+++ tests/auth/conftest.py
@@ -17,57 +17,63 @@ from un0.auth.models import Tenant, Group, User
 from un0.auth.enums import TenantType
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="function")
 def db_name() -> str:
-    return "un0_test_user"
+    return "un0_test_auth"
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="function")
 def db_url(db_name: str) -> str:
     return f"{sttngs.DB_DRIVER}://{db_name}_login:{sttngs.DB_USER_PW}@{sttngs.DB_HOST}:{sttngs.DB_PORT}/{db_name}"
 
 
-@pytest.fixture(scope="class")
-def session(db_url: str) -> sa.orm.Session:
-    return sa.orm.sessionmaker(sa.create_engine(db_url))()
+@pytest.fixture(scope="function")
+def session(db_url: str):
+    session = sa.orm.sessionmaker(sa.create_engine(db_url))()
+    yield session
+    session.close()
 
 
-@pytest.fixture(scope="class")
-def populate_db(
+@pytest.fixture(scope="function")
+def data_for_tests(
     session: sa.orm.Session, mock_superuser_session_variables, db_name
-) -> dict[Any, dict[str, Any]]:
+) -> Generator[Any, Any, Any]:
     users = []
     session.execute(sa.text(mock_superuser_session_variables))
     session.execute(sa.text(set_role_writer(db_name=db_name)))
-    cutomer_data = [
+    tenant_data = [
         ["Acme Inc.", TenantType.ENTERPRISE],
         ["Nacme Corp", TenantType.CORPORATE],
         ["Coyote LLP", TenantType.SMALL_BUSINESS],
         ["Birdy", TenantType.INDIVIDUAL],
     ]
     tenants = [
-        Tenant(name=name, tenant_type=tenant_type) for name, tenant_type in cutomer_data
+        Tenant(name=name, tenant_type=tenant_type) for name, tenant_type in tenant_data
     ]
     session.add_all(tenants)
     session.commit()
     result = session.execute(sa.select(Tenant))
     db_tenants = result.scalars().all()
-    tenant_dict = {c.name: c.id for c in db_tenants}
+    tenant_dict = {
+        t.name: {"id": t.id, "tenant_type": t.tenant_type} for t in db_tenants
+    }
 
     result = session.execute(sa.select(Group))
     db_groups = result.scalars().all()
-    group_dict = {g.name: g.id for g in db_groups}
+    group_dict = {g.name: {"id": g.id} for g in db_groups}
 
     for tenant in db_tenants:
         tenant_name = tenant.name.split(" ")[0].lower()
+        tenant_id = tenant_dict.get(tenant.name).get("id")
+        default_group_id = group_dict.get(tenant.name).get("id")
         users.append(
             User(
                 email=f"{'admin'}@{tenant_name}.com",
                 handle=f"{tenant_name}_admin",
                 full_name=f"{tenant.name} Admin",
                 is_tenant_admin=True,
-                tenant_id=tenant_dict.get(tenant.name),
-                default_group_id=group_dict.get(tenant.name),
+                tenant_id=tenant_id,
+                default_group_id=default_group_id,
             )
         )
         if tenant.tenant_type == TenantType.ENTERPRISE:
@@ -84,8 +90,8 @@ def populate_db(
                     email=f"{'User'}{u}@{tenant_name}.com",
                     handle=f"{tenant_name}_user{u}",
                     full_name=f"{tenant.name} User{u}",
-                    tenant_id=tenant_dict.get(tenant.name),
-                    default_group_id=group_dict.get(tenant.name),
+                    tenant_id=tenant_id,
+                    default_group_id=default_group_id,
                 )
             )
     session.add_all(users)
@@ -102,4 +108,8 @@ def populate_db(
         }
         for u in db_users
     }
-    return {"users": user_dict, "tenants": tenant_dict, "groups": group_dict}
+    yield {"users": user_dict, "tenants": tenant_dict, "groups": group_dict}
+    session.execute(sa.delete(User).where(User.id != ""))
+    session.execute(sa.delete(Group).where(Group.id != ""))
+    session.execute(sa.delete(Tenant).where(Tenant.id != ""))
+    session.commit()
